12372c9c9d0dde393edc8ebe56ea11cd7c3c2678
SpudGambler <jaimepalozgm@gmail.com>
2022-04-20 13:10:31 -0500

    Corrections

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ mongoose.connect(
     if (err) {
       throw err;
     } else {
-      console.log("Success connection to mongo");
+      console.log("Success connection with mongo");
       app.listen(PORT_SERVER, () => {
         console.log("##############");
         console.log("###API REST###");
         console.log("##############");
-        console.log(`http://${IP_SERVER}:${PORT_DB}/api/${API_VERSION}/`);
+        console.log(`http://${IP_SERVER}:${PORT_SERVER}/api/${API_VERSION}/`);
       });
     }
   }
