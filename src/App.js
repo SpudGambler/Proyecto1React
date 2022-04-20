ea64f31ef2cd81a7cdb11c6ddc94c07da8cb489f
SpudGambler <jaimepalozgm@gmail.com>
2022-04-20 13:02:02 -0500

    Excercice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Alert, Card, DatePicker } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Componentes Ant Desing</h1>
+      <span>Programación III</span>
+      <div>
+        <Alert message='Success Text' type='success' />
+        <Alert message='Info Text' type='info' />
+        <Alert message='Warning Text' type='warning' />
+        <Alert message='Error Text' type='error' />
+      </div>
+      <div>
+        <label>Selecciona una fecha</label>
+        <DatePicker onchange='{date_test}'></DatePicker>
+      </div>
+      <div>
+        <Card
+          size='small'
+          title='Tarjeta de presentación'
+          extra={<a href='/'>Ver más</a>}
+          style={{ width: 300 }}>
+          <p>Jaime Alonso Parra</p>
+          <p>Mg. Gestión y dev de proyectos de Software </p>
+          <p>
+            <small>jaimea.parral@autonoma.edu.co</small>
+          </p>
+        </Card>
+      </div>
     </div>
   );
 }
